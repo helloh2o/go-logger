4799911b53a648bc02d3bf3c9ef2c8ee9e0ef712
codin <codin@qq.com>
2016-01-05 14:32:21 +0800

    fat

--- logger_test.go
+++ logger_test.go
@@ -2,24 +2,24 @@ package logger
 
 import (
 	"strconv"
-	"testing"
 	"sync"
+	"testing"
 )
 
 var wg sync.WaitGroup
 
 func log(i int) {
 	Debug("Debug>>>>>>>>>>>>>>>>>>>>>>" + strconv.Itoa(i))
-	Debugf("Debugf>>>>>>>>>>>>>>>>>>>>>> %d",i)
+	Debugf("Debugf>>>>>>>>>>>>>>>>>>>>>> %d", i)
 
 	Info("Info>>>>>>>>>>>>>>>>>>>>>>>>>" + strconv.Itoa(i))
-	Debugf("Infof>>>>>>>>>>>>>>>>>>>>>> %d",i)
+	Debugf("Infof>>>>>>>>>>>>>>>>>>>>>> %d", i)
 
 	Warn("Warn>>>>>>>>>>>>>>>>>>>>>>>>>" + strconv.Itoa(i))
-	Warnf("Warnf>>>>>>>>>>>>>>>>>>>>>> %d",i)
+	Warnf("Warnf>>>>>>>>>>>>>>>>>>>>>> %d", i)
 
 	Error("Error>>>>>>>>>>>>>>>>>>>>>>>>>" + strconv.Itoa(i))
-	Errorf("Errorf>>>>>>>>>>>>>>>>>>>>>> %d",i)
+	Errorf("Errorf>>>>>>>>>>>>>>>>>>>>>> %d", i)
 
 	//	Fatal("Fatal>>>>>>>>>>>>>>>>>>>>>>>>>" + strconv.Itoa(i))
 	//	Fatalf("Fatal>>>>>>>>>>>>>>>>>>>>>>>>>%d", i)
@@ -31,7 +31,7 @@ func TestMain(t *testing.M) {
 	Init(".", "filename", DEBUG)
 
 	//指定是否控制台打印，默认为false
-//	SetConsole(true)
+	//	SetConsole(true)
 
 	for i := 10; i > 0; i-- {
 		wg.Add(1)
